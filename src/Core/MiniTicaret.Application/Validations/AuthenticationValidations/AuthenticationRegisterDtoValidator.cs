0603e8ae7caa2ded33773b80b6d99216040c718f
esmerqedirli <smrqdirli14@gmail.com>
2025-07-02 11:40:33 -0700

    created authentication endpoint

---
+++ src/Core/MiniTicaret.Application/Validations/AuthenticationValidations/AuthenticationRegisterDtoValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using MiniTicaret.Application.DTOs.AuthenticationDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniTicaret.Application.Validations.AuthenticationValidations;
+
+public class AuthenticationRegisterDtoValidator:AbstractValidator<AuthenticationRegisterDto>
+{
+    public AuthenticationRegisterDtoValidator()
+    {
+        RuleFor(x => x.FullName)
+            .NotEmpty().WithMessage("Ad boş ola bilməz");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email boş ola bilməz")
+            .EmailAddress().WithMessage("Email düzgün deyil");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Şifrə boş ola bilməz")
+            .MinimumLength(6).WithMessage("Şifrə minimum 6 simvol olmalıdır");
+
+        RuleFor(x => x.Role)
+            .NotEmpty().WithMessage("Rol seçilməlidir");
+    }
+}
